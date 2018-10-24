3cad0d0ea7854d49b5588dbb829b6d13b83f39e4
WuXiLing <liangxiaofeng1989@hotmail.com>
2018-10-24 17:26:57 +0800

    异常处理

--- WXL_PARENT/WXL_CORE/src/main/java/com/wxl/common/rest/RestCode.java
+++
@@ -1,6 +0,0 @@
-package com.wxl.common.rest;
-
-public class RestCode {
-
-	public final static int SUCCESS = 200;
-}
--- WXL_PARENT/WXL_CORE/src/main/java/com/wxl/common/rest/RestReponseMsg.java
+++ WXL_PARENT/WXL_CORE/src/main/java/com/wxl/common/rest/RestReponseMsg.java
@@ -1,41 +1,61 @@
 package com.wxl.common.rest;
 
-public class RestReponseMsg<T> implements java.io.Serializable {
+import java.util.Objects;
+
+import com.alibaba.fastjson.JSON;
+
+public class RestReponseMsg implements java.io.Serializable {
 
 	private static final long serialVersionUID = -6016291525217752497L;
+
+	private int status;
+
+	private Object reponseBody;
+
+	private String message;
+
 	
-	private int code;
-
-	private T reponseBody;
-
-	private String msg;
-
-	public int getCode() {
-		return code;
+	public RestReponseMsg() {
+		super();
 	}
 
-	public void setCode(int code) {
-		this.code = code;
+	public RestReponseMsg(Integer status, String message, Object data) {
+		this.status = status;
+		this.message = message;
+		this.reponseBody = data;
+		
 	}
 
-	public String getMsg() {
-		return msg;
+	public int getStatus() {
+		return status;
 	}
 
-	public void setMsg(String msg) {
-		this.msg = msg;
+	public void setStatus(int status) {
+		this.status = status;
 	}
 
-	public T getReponseBody() {
+	public Object getReponseBody() {
 		return reponseBody;
 	}
 
-	public void setReponseBody(T reponseBody) {
+	public void setReponseBody(Object reponseBody) {
 		this.reponseBody = reponseBody;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 	@Override
 	public String toString() {
-		return "RestReponseMsg [code=" + code + ", reponseBody=" + reponseBody + ", msg=" + msg + "]";
+
+		if (Objects.isNull(this.reponseBody)) {
+			this.setReponseBody(new Object());
+		}
+		return JSON.toJSONString(this);
 	}
 }
